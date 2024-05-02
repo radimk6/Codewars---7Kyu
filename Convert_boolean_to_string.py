8a3ab2d50cff467aba1da88957e2c8ac7c2e3892
radimk6 <kubelaradim@gmail.com>
2024-05-02 06:15:27 +0200

    Create Convert_boolean_to_string.py
    Function which convert the given boolean value into its string representation.

---
+++ Convert_boolean_to_string.py
@@ -0,0 +1,12 @@
+# Implement a function which convert the given boolean value into its string representation.
+
+# Note: Only valid inputs will be given.
+
+# def basic_test_cases():
+#         test.assert_equals(boolean_to_string(True), "True")
+#         test.assert_equals(boolean_to_string(False), "False")
+
+def boolean_to_string(b):
+  return str(b)
+
+print(boolean_to_string(False))
