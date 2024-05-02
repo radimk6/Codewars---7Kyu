4521840bf538cff7042d0f90e6a8cb891f681869
radimk6 <kubelaradim@gmail.com>
2024-05-02 06:05:59 +0200

    Create Frog's_Dinner.py
    Write a function which returns a summation of the number n

---
+++ Frog's_Dinner.py
@@ -0,0 +1,35 @@
+# Thanks for checking out my kata - in the below problem - the highest n can be is 200.
+
+# Example summation of a number - summation of the number 5 1+2+3+4+5 = 15 summation of the number 6 1+2+3+4+5+6 = 21
+
+# You are sat with two frogs on a log, Chris and Tom. They are arguing about who ate the most flies (Poor flies, but what you going to do!). Chris says "I ate the summation of n number of flies!".
+
+# Tom replies "Take half the number you ate then round it down and work out the summation of that, that is how many I ate"!
+
+# Cat then hops onto the log looking pleased with herself "Well, I ate the summation of both your dinners combined." Loz who came late to this meeting of amphibians is very confused, he asks "So how many did each of you eat?"
+
+# Write a function called frogContest which returns a string "Chris ate {number} flies, Tom ate {number} flies and Cat ate {number} flies"
+
+# {number} is the integer value of the amount of flies eaten by each.
+
+# def test_case():
+#         test.assert_equals(frog_contest(5), "Chris ate 15 flies, Tom ate 28 flies and Cat ate 946 flies")
+#         test.assert_equals(frog_contest(8), "Chris ate 36 flies, Tom ate 171 flies and Cat ate 21528 flies")
+#     @test.it("Testing for 0")
+# def test_zero():
+#         test.assert_equals(frog_contest(0), "Chris ate 0 flies, Tom ate 0 flies and Cat ate 0 flies")
+        
+def frog_contest(flies):
+    chris = 0
+    tom = 0
+    cat = 0
+    for ch in range(1, flies + 1):
+        chris += ch
+    for t in range(1, (chris // 2) + 1):
+        tom += t
+    for c in range(1, (chris + tom) + 1):
+        cat += c
+        
+    return f"Chris ate {chris} flies, Tom ate {tom} flies and Cat ate {cat} flies"
+
+print(frog_contest(5))
